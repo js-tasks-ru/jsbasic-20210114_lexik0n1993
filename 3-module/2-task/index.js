d3ed20a5c685f9a23ac115e7fffbf92ae1965e2e
LeXik0N <lexik0n1993@yandex.ru>
2021-01-26 02:06:36 +0600

    Module 3, Tasks 1-5

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,5 +5,5 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  // ваш код...
+  return arr.filter(number => number>= a && number <= b);
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,15 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+    .split(',')
+    .join(' ')
+    .split(' ')
+    .map(el => parseFloat(el))
+    .filter(el => !isNaN(el));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers),
+  }
 }
