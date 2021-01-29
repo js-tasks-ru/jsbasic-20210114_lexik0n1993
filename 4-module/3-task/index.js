3132d3f33f25a338f46ea83b7e2d2546726225bf
LeXik0N <lexik0n1993@yandex.ru>
2021-01-29 15:20:44 +0600

    Module 4, Tasks 1-3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,30 @@
  * @param {Element} table
  */
 function highlight(table) {
+  const rows = Array.from(table.tBodies[0].rows);
+
+  rows.forEach(row => {
+    if (+row.cells[1].innerText < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+
+    if (row.cells[2].innerText === 'm') {
+      row.classList.add('male');
+    } else {
+      row.classList.add('female');
+    }
+
+    const status = row.cells[3].dataset.available;
+    switch (status) {
+      case 'true':
+        row.classList.add('available');
+        break;
+      case 'false':
+        row.classList.add('unavailable');
+        break;
+      default:
+        row.setAttribute('hidden', true);
+        break;
+    }
+  });
 }
