154755978dc954d7d9fa40ca884de7391a70bfdb
LeXik0N <lexik0n1993@yandex.ru>
2021-02-08 18:58:46 +0600

    Updated Tasks 2-3, Module 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,13 +2,5 @@ function toggleText() {
   const btn = document.querySelector('.toggle-text-button');
   const text = document.querySelector('#text');
 
-  btn.addEventListener('click', () => {
-    const isTextVisible = text.getAttribute('hidden') || false;
-
-    if (isTextVisible) {
-      text.removeAttribute('hidden');
-    } else {
-      text.setAttribute('hidden', true);
-    }
-  });
+  btn.addEventListener('click', () => text.hidden = !text.hidden);
 }
