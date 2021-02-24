9d92b671ef1451114430c72fd7d7caae12cd1c1e
LeXik0N <lexik0n1993@yandex.ru>
2021-02-24 22:04:57 +0600

    Added 2 Task, Module 8

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,67 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.render();
+  }
+
+  render() {
+    const productGridHtml = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+
+    this.elem = productGridHtml;    
+    this.addProductCards(this.products);
+  }
+
+  addProductCards(products) {
+    const gridContainer = this.elem.querySelector(`.products-grid__inner`);
+
+    products.forEach(product => {
+      const productCard = new ProductCard(product);
+      gridContainer.insertAdjacentElement(`beforeend`, productCard.elem);
+    });
+  }
+
+  clearAllCards() {
+    this.elem.innerHTML = `<div class="products-grid__inner"></div>`;
+  }
+
+  filterCards({
+    noNuts = false,
+    vegeterianOnly = false,
+    maxSpiciness = 4,
+    category = ''
+  }) {
+    let filteredProducts = this.products;
+
+    if (noNuts) {
+      filteredProducts = filteredProducts.filter(product => !product.nuts || product.nuts === false);
+    }
+
+    if (vegeterianOnly) {
+      filteredProducts = filteredProducts.filter(product => product.vegeterian === true);
+    }
+
+    if (category && category.trim() !== '') {
+      filteredProducts = filteredProducts.filter(product => product.category === category);
+    }
+
+    filteredProducts = filteredProducts.filter(product => {
+      return product.spiciness <= maxSpiciness
+    });
+
+    this.clearAllCards();
+    this.addProductCards(filteredProducts);
+  }
+
+  updateFilter(filters) {
+    for (let key in filters) {
+      this.filters[key] = filters[key];
+    }
+
+    this.filterCards(this.filters);
   }
 }
